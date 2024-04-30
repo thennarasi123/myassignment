fcb03eb8669ef22dc2d26b85e8feaf0c89f5da02
thennarasi123 <168320616+thennarasi123@users.noreply.github.com>
2024-04-30 02:19:43 -0700

    Add files via upload
    week1

---
+++ day1/array.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class array {
+	public static void main(String[] args) {
+		int arr1[]= {3,2,11,4,6,7};
+		int arr2[]= {1,2,8,4,9,7};
+		System.out.println("Intersection of the two array:");
+		for(int i=0;i<arr1.length;i++) {			
+		for(int j=0;j<arr2.length;j++) {
+		if(arr1[i]==arr2[j]) {
+			System.out.println(arr1[i]);
+			}
+		}
+		}
+	}
+}
